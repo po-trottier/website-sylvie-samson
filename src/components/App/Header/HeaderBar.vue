ee396575b6d3c4188b0a93589cb7df4a0b1eacf3
Pierre-Olivier Trottier <po.trottier@gmail.com>
2020-01-10 22:38:52 -0500

    Started Building the Home Page Layout

---
+++ src/components/App/Header/HeaderBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app-bar
+    app
+    color="white"
+    height="100">
+    <div class="d-flex w-100">
+      <v-img
+        contain
+        alt="Sylvie Samson Logo"
+        class="shrink mr-5"
+        src="@/assets/logo.png"
+        width="50" />
+
+      <div style="display: grid;">
+        <span class="headline text-uppercase"><b>Sylvie Samson</b></span>
+        <span>Travailleuse Socale</span>
+      </div>
+
+      <v-spacer />
+
+      <div style="display: grid;">
+        <span class="text-uppercase"><b>Language</b></span>
+        <v-row>
+          <v-btn
+            href="/"
+            small
+            text>
+            EN
+          </v-btn>
+          <span>|</span>
+          <v-btn
+            href="/"
+            small
+            text>
+            FR
+          </v-btn>
+        </v-row>
+      </div>
+    </div>
+    <div />
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'HeaderBar',
+};
+</script>
+
+<style scoped>
+  .w-100 {
+    width: 100%;
+  }
+</style>
