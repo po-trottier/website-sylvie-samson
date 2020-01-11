17cc81718e6409a6abc11eb320908a04b1300590
Pierre-Olivier Trottier <po.trottier@gmail.com>
2020-01-10 19:40:37 -0500

    Added i18n Plugin + Configured ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,43 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/arrow-spacing': 'error',
+    'vue/camelcase': 'error',
+    'vue/key-spacing': 'error',
+    'vue/attributes-order': 'off',
+    'vue/order-in-components': 'off',
+    'vue/require-prop-types': 'off',
+    'no-plusplus': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    'no-prototype-builtins': 'off',
+    'no-trailing-spaces': 'warn',
+    'nonblock-statement-body-position': [
+      'error',
+      'below',
+    ],
+    'brace-style': [
+      'warn',
+      '1tbs',
+      {
+        allowSingleLine: false,
+      },
+    ],
+    'linebreak-style': [
+      'warn',
+      'windows',
+    ],
+    'vue/html-closing-bracket-newline': [
+      'warn',
+      {
+        singleline: 'never',
+        multiline: 'never',
+      },
+    ],
   },
   parserOptions: {
     parser: 'babel-eslint',
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-Vue.config.productionTip = false;
+import i18n from '@/plugins/i18n';
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App),
 }).$mount('#app');
