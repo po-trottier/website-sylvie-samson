5bca100f1e6a1cb7bd5d8759d8e37825a88ce3dc
Pierre-Olivier Trottier <po.trottier@gmail.com>
2020-01-15 01:42:57 -0500

    Added Compression and Obfuscation to the Webpack Config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,35 @@
+const Obfuscator = require('webpack-obfuscator');
+const Compression = require('compression-webpack-plugin');
+
+const dev = {
+  plugins: [
+    new Compression({
+      cache: true,
+      test: /\.(html|js|css|svg)/i,
+    }),
+    new Obfuscator({
+      disableConsoleOutput: true,
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      automaticNameDelimiter: '-',
+    },
+  },
+  output: {
+    chunkFilename: 'js/[name].js',
+    filename: 'js/[name].js',
+  },
+  devtool: '(none)',
+};
+
 module.exports = {
+  configureWebpack: process.env.NODE_ENV === 'production' ? dev : {
+    devServer: {
+      hot: true,
+    },
+  },
   pluginOptions: {
     i18n: {
       locale: 'fr',
@@ -10,4 +41,5 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  productionSourceMap: false,
 };
